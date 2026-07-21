f8ffab083c76e46d57e67df5eeec74504d04a592
root <root@ip-172-31-4-142.ap-south-1.compute.internal>
2026-07-21 13:23:01 +0000

    Update shop name in dev

--- src/app/shops/page.tsx
+++ src/app/shops/page.tsx
@@ -5,7 +5,7 @@ import React from "react";
 
 const shops = [
   {
-    title: "Devops Fucking Books Shop",
+    title: "Devops Fucking Books",
     image: "/icons/books.png",
     link: "/shops/books",
   },
